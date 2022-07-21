9d532e581c82dbae43354f8b35f15a9924339d3c
Murat Altun <mraltun@gmail.com>
2022-07-21 16:06:59 +0300

    Added FormRow component and add it to Register page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,5 @@
 // Import and export every component in this file. Then import all those components in single line as named imports from this folder.
 import Logo from "./Logo";
+import FormRow from "./FormRow";
 
-export { Logo };
+export { Logo, FormRow };
