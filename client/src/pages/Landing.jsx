3de3443c3dce27ee74a175dd13d4a856d5236dc2
Murat Altun <mraltun@gmail.com>
2022-07-21 14:44:37 +0300

    Added Landing page

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt='Jobify' className='logo' />
+      </nav>
+      <div className='container page'>
+        {/* Info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto
+            impedit quo et. Quam cum nulla itaque, at vel id voluptatum
+            perferendis officiis distinctio in ex ipsum velit numquam totam
+            sequi rem ratione magni corrupti enim eius! Incidunt ipsum
+            perferendis magni.
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        {/* Image */}
+        <img src={main} alt='Job Hunt' className='img main-img' />
+      </div>
+    </main>
+  );
+};
+
+export default Landing;
