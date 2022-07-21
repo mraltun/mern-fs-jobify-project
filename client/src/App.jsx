dde1d3331b10bb9a798606e64cf05033805f1f42
Murat Altun <mraltun@gmail.com>
2022-07-21 15:22:44 +0300

    Added RRD and setup basic routes inside App file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Import pages
 import Landing from "./pages/Landing";
 
 const App = () => {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>Dashboard</div>} />
+        <Route path='/register' element={<div>Register</div>} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='*' element={<div>Error</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- client/src/components/index.jsx
+++
@@ -1,4 +0,0 @@
-// Import and export every component in this file. Then import all those components as named imports from this  folder.
-import Logo from "./Logo";
-
-export { Logo };
