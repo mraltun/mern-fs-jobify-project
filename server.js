dbf213f8c0ace5001fb7bd43656908dfa1cb259f
Murat Altun <mraltun@gmail.com>
2022-07-22 14:12:30 +0300

    Added not-found middleware

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Using ES6 imports & exports instead require from CommonJS. Add ("type": "module") to package.json or change file extensions to ".mjs"
 import express from "express";
+// Middlewares. Adding file extension ".js" at the end is required for ES6 imports
+import notFoundMiddleware from "./middleware/not-found.js";
+
 const app = express();
 
 // Home route
@@ -7,7 +10,9 @@ app.get("/", (req, res) => {
   res.send("Welcome!");
 });
 
-const port = process.env.PORT || 5000;
+app.use(notFoundMiddleware);
+
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port} `);
