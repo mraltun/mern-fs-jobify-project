fd3f905be140ffdb0f469397d5f8e12e1111bf3c
Murat Altun <mraltun@gmail.com>
2022-07-22 16:30:23 +0300

    Added User model and schema

---
+++ models/User.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+// The structure for the User
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minLength: 3,
+    maxLength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide email",
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minLength: 6,
+  },
+  lastName: {
+    type: String,
+    maxLength: 20,
+    trim: true,
+    default: "lastName",
+  },
+  location: {
+    type: String,
+    maxLength: 20,
+    trim: true,
+    default: "my city",
+  },
+});
+
+// Setup the User model from schema above and export it
+export default mongoose.model("User", UserSchema);
