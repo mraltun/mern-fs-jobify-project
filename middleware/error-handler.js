ae1e27649e7e9fa2434d0089e3f1bc23a2a70fc1
Murat Altun <mraltun@gmail.com>
2022-07-23 13:35:17 +0300

    Missing fields error implemented

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -9,6 +9,16 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: "Something went wrong, try again later",
   };
 
+  // Missing fields error. Get the name of the error from err object, update status code and the message.
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message;
+    // Combine multiple error messages inside an object, then loop through them to join all in a single string.
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+  }
+
   res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
