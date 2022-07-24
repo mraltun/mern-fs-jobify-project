d77bf600c8dcc1c9c4285f7e56b6a98e61621c42
Murat Altun <mraltun@gmail.com>
2022-07-24 15:22:06 +0300

    Added unauthenticated error handler

--- errors/index.js
+++ errors/index.js
@@ -1,5 +1,6 @@
 // Import and export all error classes from here
 import BadRequestError from "./bad-request.js";
 import NotFoundError from "./not-found.js";
+import UnAuthenticatedError from "./unauthenticated.js";
 
-export { BadRequestError, NotFoundError };
+export { BadRequestError, NotFoundError, UnAuthenticatedError };
---
+++ errors/unauthenticated.js
@@ -0,0 +1,12 @@
+// Import HTTP Status Codes
+import { StatusCodes } from "http-status-codes";
+import CustomAPIError from "./custom-api.js";
+
+class UnAuthenticatedError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+export default UnAuthenticatedError;
