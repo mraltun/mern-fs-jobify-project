117b02a0ec316ff696380ce7c4f4d1a80de069b3
Murat Altun <mraltun@gmail.com>
2022-07-24 15:15:31 +0300

    Added Morgan logger middleware

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // Using ES6 imports & exports instead require from CommonJS. Add ("type": "module") to package.json or change file extensions to ".mjs"
 import express from "express";
-
 const app = express();
 // Dotenv for the loading environment variables from a.env file into "process.env"
 import dotenv from "dotenv";
@@ -13,11 +12,16 @@ import jobsRouter from "./routes/jobsRoutes.js";
 // Middlewares. Adding file extension ".js" at the end is required for ES6 imports
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
-// Built-in middleware for JSON that we pass in post requests in the controllers.
-app.use(express.json());
-
 // Express async errors for removing try/catch in our async functions
 import "express-async-errors";
+// HTTP request logger middleware
+import morgan from "morgan";
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+// Built-in middleware for JSON that we pass in post requests in the controllers.
+app.use(express.json());
 
 // Dummy home route
 app.get("/api/v1", (req, res) => {
