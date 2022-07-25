de49b333999743e70a51e80c14339e31b267e189
Murat Altun <mraltun@gmail.com>
2022-07-25 11:50:05 +0300

    Added boilerplate for Dashboard pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Import pages
-import { Dashboard, Register, Landing, Error } from "./pages";
+import { Register, Landing, Error } from "./pages";
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from "./pages/dashboard";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/' element={<div>Dashboard</div>} />
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,8 +1,7 @@
 // Import and export every page in this file. Then import all those pages in single line as named imports from this folder.
 
-import Dashboard from "./Dashboard";
 import Landing from "./Landing";
 import Register from "./Register";
 import Error from "./Error";
 
-export { Dashboard, Error, Landing, Register };
+export { Error, Landing, Register };
