7a79c675417a16a324fadf7477f49fe2da1bc49d
Murat Altun <mraltun@gmail.com>
2022-07-25 18:04:52 +0300

    Finished SmallSidebar and Added NavLinks component

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { useAppContext } from "../context/appContext";
+import NavLinks from "./NavLinks";
+// Import stylesheet
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import Logo from "./Logo";
 
 const SmallSidebar = () => {
-  return <Wrapper>SmallSidebar</Wrapper>;
+  const { showSidebar, toggleSidebar } = useAppContext();
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className='content'>
+          <button type='button' className='close-btn' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
