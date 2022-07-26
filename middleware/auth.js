a350c48b4a2e59dce42f99ca03802f0d89c9965f
Murat Altun <mraltun@gmail.com>
2022-07-26 12:48:50 +0300

    auth complete

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,27 @@
+import jwt from "jsonwebtoken";
 import { UnAuthenticatedError } from "../errors/index.js";
 
 // Authenticate user then next it to controller
 const auth = async (req, res, next) => {
+  // Check the header
   const authHeader = req.headers.authentication;
 
-  if (!authHeader) {
-    throw new UnAuthenticatedError("Authentication invalid");
+  // If there is problem with the header show error
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new UnAuthenticatedError("Authentication Unvalid");
   }
-  next();
+  // Split the Bearer and the token, then take the token.
+  const token = authHeader.split(" ")[1];
+
+  try {
+    // Verify token by token and the secret we have in the env file
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // Add a user property with userId (_id) object
+    req.user = { userId: payload.userId };
+    next();
+  } catch (error) {
+    throw new UnAuthenticatedError("Authentication Iailed");
+  }
 };
 
 export default auth;
