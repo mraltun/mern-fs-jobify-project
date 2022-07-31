424e27d4d7978c53ba142b8914e0e29b7724c36f
Murat Altun <mraltun@gmail.com>
2022-07-31 12:59:29 +0300

    Added JobsContainer component, Loading and Job components done

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Job = () => {
-  return <div>Job</div>;
+const Job = ({ company }) => {
+  return <h5>{company}</h5>;
 };
 
 export default Job;
--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppContext } from "../context/appContext";
+import Loading from "./Loading";
+import Job from "./Job";
+import Wrapper from "../assets/wrappers/JobsContainer";
 
 const JobsContainer = () => {
-  return <div>JobsContainer</div>;
+  const { getJobs, jobs, isLoading, page, totalJobs } = useAppContext();
+
+  useEffect(() => {
+    getJobs();
+  }, []);
+
+  if (isLoading) {
+    return <Loading center />;
+  }
+
+  if (jobs.length === 0) {
+    return <Wrapper>No jobs to display..</Wrapper>;
+  }
+
+  return (
+    <Wrapper>
+      <h5>
+        {/* If there is more than one job, make it "jobs" */}
+        {totalJobs} job{jobs.length > 1 && "s"} found
+      </h5>
+      <div className='jobs'>
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+      {/* pagination buttons */}
+    </Wrapper>
+  );
 };
 
 export default JobsContainer;
