840f7653ae5f2478a10b95ec2906b8001430927a
Murat Altun <mraltun@gmail.com>
2022-08-02 14:29:25 +0300

    Added BarChart and AreaChart empty components. Updated ChartsContainer

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import BarChart from "./BarChart";
+import AreaChart from "./AreaChart";
+import { useAppContext } from "../context/appContext";
+// Stylesheet
+import Wrapper from "../assets/wrappers/ChartsContainer";
 
 const ChartsContainer = () => {
-  return <div>ChartsContainer</div>;
+  // Toggle state
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
+
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 };
 
 export default ChartsContainer;
