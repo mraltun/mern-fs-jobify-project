25c93f6f2dccc185b9b1e82db3dd396018550c6c
Murat Altun <mraltun@gmail.com>
2022-08-02 12:13:54 +0300

    Updated StatItem component

--- client/src/components/StatItem.jsx
+++ client/src/components/StatItem.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+// Stylesheets
+import Wrapper from "../assets/wrappers/StatItem";
 
-const StatItem = () => {
-  return <div>StatItem</div>;
+const StatItem = ({ count, title, icon, color, bcg }) => {
+  return (
+    // We pass colors as props to css class inside Wrapper: color:${(props) => props.color}
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className='count'>{count}</span>
+        <span className='icon'>{icon}</span>
+      </header>
+      <h5 className='title'>{title}</h5>
+    </Wrapper>
+  );
 };
 
 export default StatItem;
