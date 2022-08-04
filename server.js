da182442b6081d95e7c2139371e99182571a60d5
Murat Altun <mraltun@gmail.com>
2022-08-04 11:39:36 +0300

    Added imports for filename system and example env files

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 // Dotenv for the loading environment variables from a.env file into "process.env"
 import dotenv from "dotenv";
 dotenv.config();
+// Imports for the directories. We need these because we are using ES6 so it's different than common js
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 // MongoDB connection and authenticateUser
 import connectDB from "./db/connect.js";
 // Import routers
@@ -25,15 +29,20 @@ if (process.env.NODE_ENV !== "production") {
 // Built-in middleware for JSON that we pass in post requests in the controllers.
 app.use(express.json());
 
-// Dummy home route
-app.get("/api/v1", (req, res) => {
-  res.send("Welcome!");
-});
+// Dirname is not accessible by default because ES6
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// Where the static assets will be located
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // Routes from AuthRoutes
 app.use("/api/v1/auth", authRouter);
 // We do it here since we need user to authenticate for all the routes. If we needed only certain routes then we'd do it inside jobsRouter
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+// Send the index.html if the request is not the two routes above
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 // Show error for the routes that doesn't match with defined routes above
 app.use(notFoundMiddleware);
